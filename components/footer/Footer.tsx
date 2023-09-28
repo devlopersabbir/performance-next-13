003eb663536efd943682cfab9718c3c16386b033
devlopersabbir <devlopersabbir@gmail.com>
2023-09-28 06:21:24 +0600

    ✨ Footer component finished

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-white">
-      <h1 className="text-center">footer</h1>
+    <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-1 max-md:flex-col">
+      <p>Copyright © 2023 ST Sabbir | All Rights Reserved</p>
+      <div className="gap-x-9 flex">
+        <Link href="#">Terms & conditions</Link>
+        <Link href="#">Praivacy Policy</Link>
+      </div>
     </footer>
   );
 };
