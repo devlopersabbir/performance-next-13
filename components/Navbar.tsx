c177afa34e077de0ecf1c101bc740a9f47cc2b02
devlopersabbir <devlopersabbir@gmail.com>
2023-09-28 15:15:34 +0600

    Search form

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,14 +7,15 @@ const Navbar: React.FC = () => {
     <nav className="fixed flex-center top-0 z-50 w-full border-b-2 border-black-200  py-7 text-white">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href="/">
-          <Image src="/jsm-logo.svg" alt="LOGO" width={55} height={40} />
+          {/* <Image src="/jsm-logo.svg" alt="LOGO" width={55} height={40} /> */}
+          <h1 className="text-3xl">Sabbir</h1>
         </Link>
         <Image
+          className="sm:hidden"
           src="/hamburger-menu.svg"
           alt="hamburger menu"
           height={30}
           width={30}
-          className="md:hidden"
         />
         {/* menu item */}
         <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
