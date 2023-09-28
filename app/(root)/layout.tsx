97b3c29d582b47d9725b61804812dd0be2c0b0f0
devlopersabbir <devlopersabbir@gmail.com>
2023-09-28 14:52:05 +0600

    Navbar and Hero section

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/footer/Footer";
-import Navbar from "@/components/header/Navbar";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 type TLayout = {
   children: React.ReactNode;
