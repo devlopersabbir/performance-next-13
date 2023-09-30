97dbf3ee3eee4fc7a48a6e6f90cf812422a56fdf
devlopersabbir <devlopersabbir@gmail.com>
2023-09-30 22:26:27 +0600

    Download Link schema

--- sanity/schemas/resource.schema.ts
+++ sanity/schemas/resource.schema.ts
@@ -2,4 +2,27 @@ const schema = {
   name: "resource",
   title: "Resource",
   type: "document",
+  fileld: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      require,
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    },
+    {
+      name: "downloadLink",
+      title: "Download Link",
+      type: "url",
+      validation: (Rule: any) => Rule.required(),
+    },
+  ],
 };
